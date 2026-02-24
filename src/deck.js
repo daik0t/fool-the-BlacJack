e8cedd83c7021c5c33cf20b453861b408ff9fd5e
daikot <daiko4@mail.ru>
2026-02-24 16:54:04 +0700

    Deck

---
+++ src/deck.js
@@ -0,0 +1,40 @@
+class Card{
+    #suit;
+    #rank;
+
+    constructor(suit, rnak){
+        this.#suit = suit;
+        this.#rank = rank;
+    }
+}
+
+class Deck{
+    #suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+    #ranks = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    #deck = [];
+
+    constructor(){
+        for (let i = 0; i < this.#ranks.length; i++){
+            for (let j = 0;j < this.#suits.length; j++){
+                this.#deck.push(this.#suits[j] + " " + this.#ranks[i]);
+            }
+        }
+    }
+
+    getDeck() {
+        for (i in this.#deck) {
+            console.log(i);
+            
+        }
+    }
+    
+    shuffle() {
+        return; //TODO: Сделать метод пермешивания колоды
+    }
+}
+
+test = new Deck();
+
+test.getDeck;
+
